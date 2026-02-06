72c4207e825b3bcdbd474ac3329647befa0e6c77
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2026-02-06 10:17:23 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/doogoo/doogoo/DoogooApplication.java
@@ -0,0 +1,13 @@
+package com.doogoo.doogoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoogooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoogooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doogoo/doogoo/DoogooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doogoo.doogoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoogooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
